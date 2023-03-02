71a9a46631da0f4e5163d3625a6d2407c7d60a76
lubna0501 <lubnaaggarwal2001@gmail.com>
2023-03-02 15:31:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gesture/MainActivity2.java
@@ -0,0 +1,96 @@
+package com.example.gesture;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+public class MainActivity2 extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener
+
+{
+    private  GestureDetector mDetector;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDetector= new GestureDetector(this,this);
+        mDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(this.mDetector.onTouchEvent(event))
+        {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        Log.e(TAG, "onSingleTapConfirmed: " );
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        Log.e(TAG, "onDoubleTap: " );
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        Log.e(TAG, "onDoubleTapEvent: ");
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        Log.e(TAG, "onDown: " );
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        Log.e(TAG, "onShowPress: " );
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        Log.e(TAG, "onSingleTapUp: " );
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.e(TAG, "onScroll: ");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        Log.e(TAG, "onLongPress: " );
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.e(TAG, "onFling: " );
+        startActivity(new Intent(MainActivity2.this,MainActivity.class));
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        Log.e(TAG, "onPointerCaptureChanged: " );
+    }
+}
